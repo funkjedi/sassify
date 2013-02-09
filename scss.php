2a65dceee4e727179aeaa70c2d8f01983cdb0faa
Tim Robertson <funkjedi@gmail.com>
2013-02-08 17:30:19 -0800

    Adding plugin definition

---
+++ scss.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: SCSS
+Plugin URI: https://github.com/funkjedi/SCSS
+Description: Adds SCSS support to Wordpress.
+Version: 0.1.0
+Author: Tim Robertson
+Author URI: http://github.com/funkjedi
+License: GPLv2 or later
+*/
+
